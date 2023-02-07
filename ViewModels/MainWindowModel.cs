93a03e8bd83c33a85f929bd799fc5835f6db861c
Zhang Dian <54255897+zdpcdt@users.noreply.github.com>
2023-02-07 17:42:18 +0800

    misc: update to preview5

--- ViewModels/MainWindowModel.cs
+++ ViewModels/MainWindowModel.cs
@@ -1,26 +1,19 @@
-﻿using System.ComponentModel;
-using AvaSty.Models;
+﻿using AvaSty.Models;
+using CommunityToolkit.Mvvm.ComponentModel;
 
 namespace AvaSty.ViewModels;
 
-public class MainWindowViewModel : INotifyPropertyChanged
+public class MainWindowViewModel : ObservableObject
 {
     private Content _content = new ("test");
     public Content Content => _content;
-
     string _buttonText = "Click Me!";
 
     public string ButtonText
     {
         get => _buttonText;
-        set
-        {
-            _buttonText = value;
-            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(ButtonText)));
-        }
+        set => SetProperty(ref _buttonText, value);
     }
-
-    public event PropertyChangedEventHandler? PropertyChanged;
 
     public void ButtonClicked() => ButtonText = "Hello, Avalonia!";
 }
