bed311ef7ef96566120a1c907ae339f1d62df308
Zhang Dian <54255897+zdpcdt@users.noreply.github.com>
2023-03-01 09:52:40 +0800

    feat: layoutSample

---
+++ Pages/LayoutSample.axaml.cs
@@ -0,0 +1,18 @@
+﻿using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace AvaSty.Pages;
+
+public partial class LayoutSample : UserControl
+{
+    public LayoutSample()
+    {
+        InitializeComponent();
+    }
+
+    private void InitializeComponent()
+    {
+        AvaloniaXamlLoader.Load(this);
+    }
+}
