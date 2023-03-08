72fbf5bf8e98cc9083f4ccb596188f2f5d6ffc8f
Zhang Dian <54255897+zdpcdt@users.noreply.github.com>
2023-03-08 21:18:06 +0800

    feat: Template&Layout Samples

---
+++ Models/Student.cs
@@ -0,0 +1,19 @@
+﻿namespace AvaSty.Models;
+
+public class Student
+{
+    public Student()
+    {
+        FirstName = "Xi";
+        LastName = "Li";
+    }
+
+    public Student(string firstName, string lastName)
+    {
+        FirstName = firstName;
+        LastName = lastName;
+    }
+
+    public string? FirstName { get; set; }
+    public string? LastName { get; set; }
+}
---
+++ Pages/TemplateSample.axaml.cs
@@ -0,0 +1,18 @@
+﻿using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace AvaSty.Pages;
+
+public partial class TemplateSample : UserControl
+{
+    public TemplateSample()
+    {
+        InitializeComponent();
+    }
+
+    private void InitializeComponent()
+    {
+        AvaloniaXamlLoader.Load(this);
+    }
+}
